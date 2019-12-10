598fe8f9be1973b7374427b9d2b9e4d233b3ab59
Steveycat <Steveycat@users.noreply.github.com>
2019-12-10 02:12:29 +0000

    Initial commit

---
+++ Breakout.Designer.vb
@@ -0,0 +1,331 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Breakout
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pbxBall = New System.Windows.Forms.PictureBox()
+        Me.TmrGame = New System.Windows.Forms.Timer(Me.components)
+        Me.pbxBat = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox14 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblLives = New System.Windows.Forms.Label()
+        CType(Me.pbxBall, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbxBat, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbxBall
+        '
+        Me.pbxBall.BackColor = System.Drawing.Color.White
+        Me.pbxBall.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbxBall.InitialImage = Nothing
+        Me.pbxBall.Location = New System.Drawing.Point(344, 350)
+        Me.pbxBall.Name = "pbxBall"
+        Me.pbxBall.Size = New System.Drawing.Size(31, 28)
+        Me.pbxBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbxBall.TabIndex = 0
+        Me.pbxBall.TabStop = False
+        '
+        'TmrGame
+        '
+        Me.TmrGame.Enabled = True
+        Me.TmrGame.Interval = 33
+        '
+        'pbxBat
+        '
+        Me.pbxBat.BackColor = System.Drawing.Color.DarkRed
+        Me.pbxBat.Enabled = False
+        Me.pbxBat.Location = New System.Drawing.Point(303, 416)
+        Me.pbxBat.Name = "pbxBat"
+        Me.pbxBat.Size = New System.Drawing.Size(112, 22)
+        Me.pbxBat.TabIndex = 1
+        Me.pbxBat.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Black
+        Me.PictureBox2.Location = New System.Drawing.Point(115, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Black
+        Me.PictureBox3.Location = New System.Drawing.Point(218, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Black
+        Me.PictureBox4.Location = New System.Drawing.Point(321, 12)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox4.TabIndex = 5
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Black
+        Me.PictureBox5.Location = New System.Drawing.Point(424, 12)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox5.TabIndex = 6
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.Black
+        Me.PictureBox6.Location = New System.Drawing.Point(527, 12)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox6.TabIndex = 7
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.Color.Black
+        Me.PictureBox7.Location = New System.Drawing.Point(630, 12)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox7.TabIndex = 8
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox8.Location = New System.Drawing.Point(12, 66)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox8.TabIndex = 9
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox9.Location = New System.Drawing.Point(115, 66)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox9.TabIndex = 10
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox10.Location = New System.Drawing.Point(218, 66)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox10.TabIndex = 11
+        Me.PictureBox10.TabStop = False
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox11.Location = New System.Drawing.Point(321, 66)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox11.TabIndex = 12
+        Me.PictureBox11.TabStop = False
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox12.Location = New System.Drawing.Point(424, 66)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox12.TabIndex = 13
+        Me.PictureBox12.TabStop = False
+        '
+        'PictureBox13
+        '
+        Me.PictureBox13.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox13.Location = New System.Drawing.Point(527, 66)
+        Me.PictureBox13.Name = "PictureBox13"
+        Me.PictureBox13.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox13.TabIndex = 14
+        Me.PictureBox13.TabStop = False
+        '
+        'PictureBox14
+        '
+        Me.PictureBox14.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox14.Location = New System.Drawing.Point(630, 66)
+        Me.PictureBox14.Name = "PictureBox14"
+        Me.PictureBox14.Size = New System.Drawing.Size(97, 48)
+        Me.PictureBox14.TabIndex = 15
+        Me.PictureBox14.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(9, 425)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 16)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Score"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(59, 425)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(15, 16)
+        Me.lblScore.TabIndex = 17
+        Me.lblScore.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(671, 425)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 16)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Lives"
+        '
+        'lblLives
+        '
+        Me.lblLives.AutoSize = True
+        Me.lblLives.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLives.Location = New System.Drawing.Point(717, 425)
+        Me.lblLives.Name = "lblLives"
+        Me.lblLives.Size = New System.Drawing.Size(15, 16)
+        Me.lblLives.TabIndex = 19
+        Me.lblLives.Text = "3"
+        '
+        'Breakout
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkGray
+        Me.ClientSize = New System.Drawing.Size(744, 450)
+        Me.Controls.Add(Me.lblLives)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox14)
+        Me.Controls.Add(Me.PictureBox13)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.pbxBat)
+        Me.Controls.Add(Me.pbxBall)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Breakout"
+        Me.Text = "Breakout"
+        CType(Me.pbxBall, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbxBat, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pbxBall As PictureBox
+    Friend WithEvents TmrGame As Timer
+    Friend WithEvents pbxBat As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
+    Friend WithEvents PictureBox14 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblScore As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblLives As Label
+End Class
---
+++ Breakout.vb
@@ -0,0 +1,99 @@
+﻿Public Class Breakout
+    Private intDx As Integer = 4
+    Private intDy As Integer = -3
+    Private intScore As Integer = 0
+    Private intLives As Integer = 3
+    ' creates an arry of pictureboxes
+    Const Maxbricks As Integer = 13
+    Private pbxBricks(Maxbricks + 1) As PictureBox
+    Private Sub TmrGame_Tick(sender As Object, e As EventArgs) Handles TmrGame.Tick
+        Call MoveBall()
+        Call CollisionBallBat()
+        Call CollisionBallBricks()
+        Call EndOfLevel()
+    End Sub
+    Private Sub MoveBall()
+        pbxBall.Left += intDx
+        pbxBall.Top += intDy
+        ' ball bounces off the top
+        If pbxBall.Top < 0 Then
+            intDy = -intDy
+        End If
+        ' ball bounces off the left
+        If pbxBall.Left < 0 Then
+            intDx = -intDx
+        End If
+        If pbxBall.Right > Me.Width - 20 Then
+            intDx = -intDx
+        End If
+        If pbxBall.Bottom > Me.Height - 30 Then
+            intLives -= 1
+            lblLives.Text = intLives
+            pbxBall.Top = Me.Height / 2
+            If intLives = 0 Then
+                TmrGame.Enabled = False
+                MsgBox("Game Over")
+            End If
+        End If
+    End Sub
+    Private Sub Breakout_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        pbxBat.Left = e.X
+    End Sub
+    Private Sub CollisionBallBat()
+        If pbxBall.Bounds.IntersectsWith(pbxBat.Bounds) Then
+            intDy = -intDy
+            ' different between cnetre of the bat and the ball is X speed
+            Dim intcenterbat As Integer = pbxBat.Left + pbxBat.Width / 2
+            Dim intcenterball As Integer = pbxBall.Left + pbxBall.Width / 2
+            Dim intDiff As Integer = intcenterball - intcenterbat
+            intDx = intDiff / 2
+        End If
+    End Sub
+    Private Sub Breakout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Creates the bricks
+        pbxBricks(0) = PictureBox1
+        pbxBricks(1) = PictureBox2
+        pbxBricks(2) = PictureBox3
+        pbxBricks(3) = PictureBox4
+        pbxBricks(4) = PictureBox5
+        pbxBricks(5) = PictureBox6
+        pbxBricks(6) = PictureBox7
+        pbxBricks(7) = PictureBox8
+        pbxBricks(8) = PictureBox9
+        pbxBricks(9) = PictureBox10
+        pbxBricks(10) = PictureBox11
+        pbxBricks(11) = PictureBox12
+        pbxBricks(12) = PictureBox13
+        pbxBricks(13) = PictureBox14
+    End Sub
+    Private Sub CollisionBallBricks()
+        Dim i As Integer
+        For i = 0 To Maxbricks
+            ' When the ball hits a brick
+            If pbxBall.Bounds.IntersectsWith(pbxBricks(i).Bounds) Then
+                ' If the brick is visable (not been hit) then make it invisable
+                If pbxBricks(i).Visible = True Then
+                    pbxBricks(i).Visible = False
+                    ' Reverse the current direction of travel
+                    intDy = -intDy
+                    ' Score counts 1
+                    intScore += 1
+                    lblScore.Text = intScore
+                End If
+            End If
+        Next
+    End Sub
+    Private Sub EndOfLevel()
+        Dim i As Integer
+        ' Loop through all of the bricks
+        For i = 0 To Maxbricks
+            If pbxBricks(i).Visible = True Then
+                Exit Sub
+            End If
+        Next
+        ' No visable bricks found
+        For i = 0 To Maxbricks
+            pbxBricks(i).Visible = True
+        Next
+    End Sub
+End Class
